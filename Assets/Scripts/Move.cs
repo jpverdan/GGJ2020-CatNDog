0b074b2df0f08868c513e655e75c0d0e79ed7158
jpverdan <jpverdan@hotmail.com>
2020-01-31 21:47:17 -0300

    Commit inicial e movimentação básica

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+    public float moveSpeed = 2f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");       
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
