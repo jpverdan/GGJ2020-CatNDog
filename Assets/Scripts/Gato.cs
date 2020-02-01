f0028251994e4da175f518c815c64d87a4c30a19
tumati-bitbucket <jgsoaresdias@gmail.com>
2020-02-01 17:15:06 -0300

    Gato andando ate o item e quebrando

--- Assets/Scripts/Gato.cs
+++ Assets/Scripts/Gato.cs
@@ -2,18 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
+
+public enum CatStates { WALKING_AROUND, GOING_TO_BREAK }
 
 public class Gato : MonoBehaviour
 {
     private List<Item> _listOfItems = new List<Item>();
-    public float timeToBreakItemInSeconds = 5f;
+    public float timeToBreakItemInSeconds = 3f;
     private float _coolDownToBreakItem;
+    private Seeker _seeker;
+    private CatStates _catState = CatStates.WALKING_AROUND;
+
+    private Item _itemToBreak = null;
+
+    IAstarAI _agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        _agent = GetComponent<IAstarAI>();
         GetAllItems();
         _coolDownToBreakItem = timeToBreakItemInSeconds;
+        _seeker = GetComponent<Seeker>();
     }
 
 
@@ -23,19 +34,50 @@ public class Gato : MonoBehaviour
         if (_coolDownToBreakItem > 0)
         {
             _coolDownToBreakItem -= Time.deltaTime;
-        } else
+        }
+
+        switch (_catState)
         {
-            _coolDownToBreakItem = timeToBreakItemInSeconds;
-            BreakItem();
+            case CatStates.WALKING_AROUND:
+                // choose point
+                // walk to point
+                if (_coolDownToBreakItem <= 0 && _listOfItems.Count > 0)
+                {
+                    _itemToBreak = ChooseItem();
+                    _agent.destination = _itemToBreak.transform.position;
+                    _agent.SearchPath();
+                    StartCoroutine(GoingToBreakItem());
+                    _catState = CatStates.GOING_TO_BREAK;
+                }
+                break;
+            case CatStates.GOING_TO_BREAK:
+                break;
+            default:
+                break;
         }
     }
 
-    private void BreakItem()
+    IEnumerator GoingToBreakItem()
+    {
+        float _cooldown = 2f;
+        while (_agent.remainingDistance >= _agent.radius || _cooldown > 0)
+        {
+            _cooldown -= .5f;
+            yield return new WaitForSeconds(.5f);
+            print("Coroutine");
+
+        }
+        BreakItem(_itemToBreak);
+        _coolDownToBreakItem = timeToBreakItemInSeconds;
+        _catState = CatStates.WALKING_AROUND;
+    }
+
+    private void BreakItem(Item _item)
     {
         if (_listOfItems.Count == 0) return;
-
-        Item _item = _listOfItems[UnityEngine.Random.Range(0, _listOfItems.Count)];
+        
         _item.Quebrar();
+        _itemToBreak = null;
         _listOfItems.Remove(_item);
     }
 
@@ -51,5 +93,10 @@ public class Gato : MonoBehaviour
     public void ReturnItemToList(Item _item)
     {
         _listOfItems.Add(_item);
+    }
+
+    Item ChooseItem()
+    {
+        return _listOfItems[UnityEngine.Random.Range(0, _listOfItems.Count)];
     }
 }
