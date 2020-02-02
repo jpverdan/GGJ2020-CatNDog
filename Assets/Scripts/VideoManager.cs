64d92b777685c830d304618d4ac54d0139fd753c
jpverdan <jpverdan@hotmail.com>
2020-02-02 16:28:16 -0300

    Cutscene funcionando

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VideoPlayer>().loopPointReached += MudaCena;   
+    }
+
+    public void MudaCena(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(2);      
+    }
+
+}
