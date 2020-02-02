1a67a518b9d6e56344dc3ff7493aae9b0fe3affe
jpverdan <jpverdan@hotmail.com>
2020-02-02 01:02:06 -0300

    Main menu com arte

---
+++ Assets/Scripts/TimeBar/SlideChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideChange : MonoBehaviour
+{
+
+    private float timeRemaining;
+    private const float timerMax = 5f;
+    public Slider slider;
+
+    private void Start() {
+        timeRemaining = timerMax;
+    }
+
+    void Update()
+    {
+        slider.value = CalculateSliderValue();
+
+        if (timeRemaining >= 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+
+        if(timeRemaining<=0)
+        {
+            print("Dever de casa concluído!");           
+        }
+    }
+
+
+    float CalculateSliderValue()
+    {
+        return (timeRemaining   / timerMax);
+    }
+}
