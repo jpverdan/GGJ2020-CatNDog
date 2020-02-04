b0bdaf9a180ec2c17df87f9abe2a47e978c6e7c2
tumatihub <jgsoaresdias@gmail.com>
2020-02-03 23:28:36 -0300

    Ajuste no VideoPlayer para uso em webgl

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class VideoManager : MonoBehaviour
 {
+    VideoPlayer _videoPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<VideoPlayer>().loopPointReached += MudaCena;   
+        _videoPlayer = GetComponent<VideoPlayer>();
+        _videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "CutscenePequena.mp4");
+        _videoPlayer.loopPointReached += MudaCena;   
     }
 
     public void MudaCena(VideoPlayer vp)
